51147779d2d50a2082acf2ef298f68e82a4a2386
TecStylos <tecstylos@gmail.com>
2021-10-27 16:19:31 +0200

    Fixed warning

--- MarCore/src/AsmInstructions.cpp
+++ MarCore/src/AsmInstructions.cpp
@@ -4,10 +4,12 @@ namespace MarC
 {
 	const InstructionLayout& InstructionLayoutFromOpCode(BC_OpCode oc)
 	{
+	  static const InstructionLayout emptyLayout(BC_OC_NONE, false, {});
 		for (auto& il : InstructionSet)
 		{
 			if (oc == il.opCode)
 				return il;
 		}
+		return emptyLayout;
 	}
 }
