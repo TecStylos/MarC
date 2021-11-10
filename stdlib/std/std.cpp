5934d7704ecfa0377bede0acb70afe07ab7e5e88
TecStylos <tecstylos@gmail.com>
2021-11-10 20:47:54 +0100

    Added sleepms function to std lib

--- stdlib/std/std.cpp
+++ stdlib/std/std.cpp
@@ -4,6 +4,7 @@
 #include "../../MarCore/include/Interpreter.h"
 
 #include <iostream>
+#include <thread>
 
 class EF_PrintS : public MarC::ExternalFunction
 {
@@ -48,12 +49,28 @@ public:
 	}
 };
 
+class EF_SleepMS : public MarC::ExternalFunction
+{
+public:
+	using MarC::ExternalFunction::ExternalFunction;
+	PLUS_FEATURE_GET_NAME(">>std>>sleepms");
+	virtual void call(MarC::Interpreter& interpreter, MarC::ExFuncData& efd) override
+	{
+		if (efd.nParams != 1)
+			throw MarC::InterpreterError(MarC::IntErrCode::WrongExtCallParamCount, "Expected 1 parameter! Got " + std::to_string(efd.nParams) + "!");
+		if (efd.param[0].datatype != MarC::BC_DT_U_64)
+			throw MarC::InterpreterError(MarC::IntErrCode::WrongExtCallParamCount, "Expected parameter of type 'BC_DT_U_64! Got '" + MarC::BC_DatatypeToString(efd.param[0].datatype) + "'!");
+		std::this_thread::sleep_for(std::chrono::milliseconds(efd.param[0].cell.as_U_64));
+	}
+};
+
 PLUS_PERPLUGIN_DEFINE_EXTERNALS("STD-EXTENSION");
 
 void PluS::PerPlugin::initPlugin()
 {
 	pPlugin->registerFeatureFactory(FeatureFactory::create<EF_PrintS>());
 	pPlugin->registerFeatureFactory(FeatureFactory::create<EF_PrintT>());
+	pPlugin->registerFeatureFactory(FeatureFactory::create<EF_SleepMS>());
 }
 
 void PluS::PerPlugin::shutdownPlugin()
