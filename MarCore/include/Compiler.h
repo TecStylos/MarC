6db12b9e09b5bc2aee6c4bc4d2c63eb7c8632456
TecStylos <tecstylos@gmail.com>
2021-11-11 19:46:53 +0100

    Extensions now have postfixes matching their built type

--- MarCore/include/Compiler.h
+++ MarCore/include/Compiler.h
@@ -34,8 +34,8 @@ namespace MarC
 		std::string getMessage() const;
 	private:
 		Code m_code = Code::Success;
-		uint16_t m_line;
-		uint16_t m_column;
+		uint16_t m_line = 0;
+		uint16_t m_column = 0;
 		std::string m_errText = "Success!";
 		uint64_t m_sysErrLine = 0;
 		std::string m_sysErrFile = "<unspecified>";
--- MarCore/src/ExtensionLocator.cpp
+++ MarCore/src/ExtensionLocator.cpp
@@ -2,6 +2,16 @@
 
 #include "PluS/Defines.h"
 
+#if defined PLUS_BUILD_TYPE_DEBUG
+#define MARC_EXTENSION_POSTFIX "-dbg"
+#elif defined PLUS_BUILD_TYPE_RELWITHDEBINFO
+#define MARC_EXTENSION_POSTFIX "-rel"
+#elif defined PLUS_BUILD_TYPE_RELEASE
+#define MARC_EXTENSION_POSTFIX "-dist"
+#else
+#error PLUS_BUILD_TYPE_<CONFIG> NOT DEFINED!
+#endif
+
 namespace MarC
 {
 	std::map<std::string, std::set<std::string>> locateExtensions(const std::set<std::string>& baseDirs, const std::set<std::string>& extNames)
@@ -21,6 +31,9 @@ namespace MarC
 					continue;
 
 				auto stem = entry.path().stem().string();
+				if (stem.find(MARC_EXTENSION_POSTFIX) != stem.size() - strlen(MARC_EXTENSION_POSTFIX))
+					continue;
+				stem = stem.substr(0, stem.size() - strlen(MARC_EXTENSION_POSTFIX));
 
 				auto extMatch = extNames.find(stem);
 				if (extMatch == extNames.end())
