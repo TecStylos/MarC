db709b0ad8772226828cb69c723eea78e8a65862
TecStylos <tecstylos@gmail.com>
2022-02-01 21:48:38 +0100

    Fixed wrong datatype in stdlib's 'strLen'

--- runCallgrind.sh
+++ runCallgrind.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+./build.sh RelWithDebInfo
+
+rm callgrind.out.*
+
 valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./bin/RelWithDebInfo/MarCmd/MarCmd --verbose --closeonexit examples/fibonacci.mca
